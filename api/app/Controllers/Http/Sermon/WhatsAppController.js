371a2f4d739e7a7158cbb1de2a2a32bd4e31d497
Davi Coscarelli <48040161+davicoscarelli@users.noreply.github.com>
2024-08-06 14:08:45 -0300

    Update WhatsAppController.js

--- api/app/Controllers/Http/Sermon/WhatsAppController.js
+++ api/app/Controllers/Http/Sermon/WhatsAppController.js
@@ -6,14 +6,14 @@ class WhatsAppController {
   async webhook({ request, response }) {
     try {
         console.log("AAAAAAA ENTROUUUu")
-    //   const incomingMessage = request.input('Body')
-    //   const from = request.input('From').replace('whatsapp:', '')
+      const incomingMessage = request.input('Body')
+      const from = request.input('From').replace('whatsapp:', '')
 
-    //   // Process the incoming message
-    //   const reply = await MessageProcessorService.processMessage(from, incomingMessage)
+      // Process the incoming message
+      const reply = await MessageProcessorService.processMessage(from, incomingMessage)
 
-    //   // Send the response back to the user
-    //   await WhatsAppService.sendMessage(from, reply)
+      // Send the response back to the user
+      await WhatsAppService.sendMessage(from, reply)
 
       response.status(200).send('Message processed')
     } catch (error) {

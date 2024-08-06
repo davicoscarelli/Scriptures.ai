bf236090dd5cbf0c1ea1235ea92bc160161e9553
Davi Coscarelli <48040161+davicoscarelli@users.noreply.github.com>
2024-08-06 13:07:44 -0300

    Update routes.js

--- api/start/routes.js
+++ api/start/routes.js
@@ -17,4 +17,4 @@
 const Route = use('Route')
 
 Route.on('/').render('welcome')
-Route.post('/webhook/whatsapp', 'WhatsAppController.webhook')
+Route.post('/webhook/whatsapp', 'Whatsapp/WhatsAppController.webhook')

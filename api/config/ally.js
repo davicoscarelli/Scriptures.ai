053fa4e6193a4d0189a11f4124d2bcddff913ca8
Davi Coscarelli <48040161+davicoscarelli@users.noreply.github.com>
2024-08-11 01:45:11 -0300

    aaa

--- api/config/ally.js
+++ api/config/ally.js
@@ -1,10 +1,10 @@
-const { defineConfig } = use('@adonisjs/ally')
+import { defineConfig, services } from '@adonisjs/ally/build/src/AllyManager'
 
 module.exports = defineConfig({
   google: {
     driver: 'google',
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackUrl: 'http://localhost:3333/google/callback',
+    callbackUrl: 'https://scriptures-ai-6c1591cb87ac.herokuapp.com/v1/google/callback',
   },
 })

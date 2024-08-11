5a6c329ea3ac2eab9aae54326168ba2b75ba0391
Davi Coscarelli <48040161+davicoscarelli@users.noreply.github.com>
2024-08-11 18:57:12 -0300

    Update 1723413326509_user_schema.js

--- api/database/migrations/1723413326509_user_schema.js
+++ api/database/migrations/1723413326509_user_schema.js
@@ -7,10 +7,7 @@ class UserSchema extends Schema {
   up () {
     this.table('users', (table) => {
       table.string('google_id', 255).notNullable().unique() 
-      table.string('email', 254).notNullable().unique() 
       table.string('phone_number', 20).notNullable().unique()
-      table.string('username', 80).nullable().unique() 
-      table.text('avatar').nullable()
     })
   }
 

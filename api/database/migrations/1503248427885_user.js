2f51b127feffe0c572e6c94c1340948c12dde3b8
Davi Coscarelli <48040161+davicoscarelli@users.noreply.github.com>
2024-08-11 05:04:47 -0300

    a

--- api/database/migrations/1503248427885_user.js
+++ api/database/migrations/1503248427885_user.js
@@ -9,6 +9,7 @@ class UserSchema extends Schema {
       table.increments()
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
+      table.text('avatar').nullable()
       table.string('password', 60).notNullable()
       table.timestamps()
     })

a342983c7dd6d5b045aaf821515d33e68c065248
Davi Coscarelli <48040161+davicoscarelli@users.noreply.github.com>
2024-08-11 18:56:01 -0300

    aaaa

--- api/database/migrations/1723413326509_user_schema.js
+++ api/database/migrations/1723413326509_user_schema.js
@@ -5,19 +5,19 @@ const Schema = use('Schema')
 
 class UserSchema extends Schema {
   up () {
-    this.create('users', (table) => {
-      table.increments()
+    this.table('users', (table) => {
       table.string('google_id', 255).notNullable().unique() 
       table.string('email', 254).notNullable().unique() 
       table.string('phone_number', 20).notNullable().unique()
       table.string('username', 80).nullable().unique() 
       table.text('avatar').nullable()
-      table.timestamps()
     })
   }
 
   down () {
-    this.drop('users')
+    this.table('users', (table) => {
+      // reverse alternations
+    })
   }
 }
 
